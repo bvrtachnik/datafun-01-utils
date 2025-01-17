c7d876102cb75bb020ef128b4b791e5ba2486ac9
bvrtachnik <b.vrtachnik@gmail.com>
2025-01-17 17:48:29 -0500

    Update global variables

--- utils_bvrtachnik.py
+++ utils_bvrtachnik.py
@@ -29,18 +29,22 @@ import statistics  # provides mean(), stdev() and more....
 # declare a boolean variable (has a value True or False)
 # TODO: Add another or replace this with your own boolean variable
 has_international_clients: bool = True
+is_privately_held: bool = True
 
 # declare an integer variable 
 # TODO: Add or replace this with your own integer variable
 years_in_operation: int = 10
+number_of_employees: int = 30
 
 # declare a floating point variable
 # TODO: Add or replace this with your own floating point variable
 average_client_satisfaction: float = 4.7
+average_local_daily_temperature: float = 70
 
 # declare a list of strings
 # TODO: Add or replace this with your own list  
 skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
+languages_used: list = ["SQL", "GitHub", "Python"]
 
 # declare a list of numbers so we can illustrate statistics skills
 # TODO: Add or replace this with your own numeric list  
